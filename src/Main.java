58e00b64d39f17f594f389d6886708399229382f
seungjoon1011 <sg15400@gmail.com>
2025-05-30 14:35:45 +0900

    variable

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,21 @@
 public class Main {
     public static void main(String[] args) {
         int age = 25;
-
         System.out.println(age);
+
+        age = 26;
+        System.out.println(age);
+
+        long bi = 999999999;
+        System.out.println(bi);
+
+        double silsu = 0.821;
+        System.out.println(silsu);
+
+        char word = 'ㅁ';
+        System.out.println(word);
+
+        String MyName= "이승준승이승준승이승준";
+        System.out.println(MyName.length());
     }
 }
